2dfe8ffd06c2bfbade83d2158d0f3933ee926bd7
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2019-09-24 19:59:13 +0200

    add input and paragraph displaying its length

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    text: "",
+    textLength: 0
+  };
+
+  textChanged = event => {
+    const text = event.target.value;
+    this.setState({
+      text,
+      textLength: text.length
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <input
+          type="text"
+          value={this.state.text}
+          onChange={this.textChanged}
+        />
+        <p>Length of input: {this.state.textLength}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
